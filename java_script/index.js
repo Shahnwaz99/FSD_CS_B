a2fae4fc04adeaa2da54243bd0d77e064178b5fb
Shahnwaz <iamshaan03@gmail.com>
2024-11-19 23:49:45 +0530

    first commit

---
+++ java_script/index.js
@@ -0,0 +1,11 @@
+const hello = () => {
+    console.log("hello");
+  };
+  hello();
+  const student = {
+    age: 21,
+    name: "shahnwaz",
+    place: "up",
+  };
+  console.log(student);
+  console.log(age);
